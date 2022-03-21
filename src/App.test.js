a31cc9688ac716301051e9217e165fc301176cdd
DanHembery <93157414+DanHembery@users.noreply.github.com>
2022-03-21 14:00:06 +0000

    project setup complete, eslint and prettier installed

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders app title', () => {
+test("renders app title", () => {
   render(<App />);
   const linkElement = screen.getByText(/Weather App/i);
   expect(linkElement).toBeInTheDocument();
